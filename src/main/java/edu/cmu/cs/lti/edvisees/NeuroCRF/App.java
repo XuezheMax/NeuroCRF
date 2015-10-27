aaa968019c785976f88ce5c9c57bf058d6ac3c21
Max Ma <xuezhe.ma@gmail.com>
2015-10-26 22:15:41 -0400

    change for gitignore

--- src/main/java/edu/cmu/cs/lti/edvisees/NeuroCRF/App.java
+++ src/main/java/edu/cmu/cs/lti/edvisees/NeuroCRF/App.java
@@ -1,13 +1,15 @@
 package edu.cmu.cs.lti.edvisees.NeuroCRF;
 
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
     }
 }
