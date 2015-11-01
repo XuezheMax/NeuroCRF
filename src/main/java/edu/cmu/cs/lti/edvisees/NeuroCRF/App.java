d90e1b8e73b5de0c83b622f73ab45b9259ed03eb
Max Ma <xuezhe.ma@gmail.com>
2015-11-01 15:38:57 -0500

    pom

--- src/main/java/edu/cmu/cs/lti/edvisees/NeuroCRF/App.java
+++ src/main/java/edu/cmu/cs/lti/edvisees/NeuroCRF/App.java
@@ -1,15 +1,24 @@
 package edu.cmu.cs.lti.edvisees.NeuroCRF;
 
+import java.io.*;
 
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
 /**
  * Hello world!
  *
+ * add "-Dorg.nd4j.linalg.cpu.force_native=false" to JVM argument for pure java
  */
 public class App 
 {
     public static void main( String[] args ) {
 
+        INDArray nd = Nd4j.create(new float[]{1, 2}, new int[]{2});
+        INDArray nd2 = Nd4j.create(new float[]{3,4},new int[]{2, 1});
+        nd = nd2.mmul(nd);
+        System.out.println(nd.toString());
         System.out.println( "Hello World!" );
     }
 }
